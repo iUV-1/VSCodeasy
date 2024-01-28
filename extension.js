151f5b22f670a302c31adc56eabce65b4f720ecc
Nguyễn Minh Quyền <megaluigi2804@gmail.com>
2024-01-28 08:55:16 -0500

    ahh

--- extension.js
+++ extension.js
@@ -41,68 +41,68 @@ function activate(context) {
 		let whiteboard = WhiteBoardManager.getManager(); 
 		// show current panel or create new panel
 		WhiteBoardManager.createNewPanel(context);
-		
+	})
 /*
 		else {
 			WhiteBoardManager.showOldPanel();
 		}
 */
 
-		let disposable = vscode.commands.registerCommand('vscodeasy.copytoclipboard0', () => {
-			if (copyToAClipboard(0) == -1){
-				vscode.window.showErrorMessage('No highlighted text found!');
-			} else{
-				vscode.window.showInformationMessage("Copied to clipboard 0!")
-				treeDataProvider.refresh()
-			}
-			console.log(Codeasy_Clipboard)
-		})
-
-		let disposable2 = vscode.commands.registerTextEditorCommand('vscodeasy.pastefromclipboard0', (editor, edit) => {
-			pasteFromAClipboard(editor, edit, 0)
-		})
-
-		let disposable3 = vscode.commands.registerCommand('vscodeasy.openWebview', function () {
-			const panel = vscode.window.createWebviewPanel(
-				'webview', // Identifies the type of the webview. Used internally
-				'Webview Title', // Title of the panel displayed to the user
-				vscode.ViewColumn.One, // Editor column to show the new webview panel in.
-				{} // Webview options. More on these later.
-			);
-
-			panel.webview.html = getWebviewContent();
-		});
-
-		let disposable4 = vscode.commands.registerCommand('vscodeasy.copytoclipboard1', () => {
-			if (copyToAClipboard(1) == -1){
-				vscode.window.showErrorMessage('No highlighted text found!');
-			} else{
-				vscode.window.showInformationMessage("Copied to clipboard 1!")
-				treeDataProvider.refresh()
-			}
-			console.log(Codeasy_Clipboard)
-		})
-
-		let disposable5 = vscode.commands.registerTextEditorCommand('vscodeasy.pastefromclipboard1', (editor, edit) => {
-			pasteFromAClipboard(editor, edit, 1)
-		})
-
-
-		context.subscriptions.push(disposable)
-		context.subscriptions.push(disposable2)
-		context.subscriptions.push(disposable3)
-		context.subscriptions.push(disposable4)
-		context.subscriptions.push(disposable5)
-		const treeDataProvider = new MyTreeDataProvider();
-		context.subscriptions.push(vscode.window.registerTreeDataProvider('mySidebar', treeDataProvider));
-
-		// context.subscriptions.push(
-		//     vscode.window.registerWebviewViewProvider('mySidebar', new SidebarProvider(context.extensionUri))
-		// );
+	let disposable4 = vscode.commands.registerCommand('vscodeasy.copytoclipboard0', () => {
+		if (copyToAClipboard(0) == -1){
+			vscode.window.showErrorMessage('No highlighted text found!');
+		} else{
+			vscode.window.showInformationMessage("Copied to clipboard 0!")
+			treeDataProvider.refresh()
+		}
+		console.log(Codeasy_Clipboard)
 	})
 
-	context.subscriptions.push(disposable);
-	context.subscriptions.push(disposable3); 
+	let disposable5 = vscode.commands.registerTextEditorCommand('vscodeasy.pastefromclipboard0', (editor, edit) => {
+		pasteFromAClipboard(editor, edit, 0)
+	})
+
+	let disposable6 = vscode.commands.registerCommand('vscodeasy.openWebview', function () {
+		const panel = vscode.window.createWebviewPanel(
+			'webview', // Identifies the type of the webview. Used internally
+			'Webview Title', // Title of the panel displayed to the user
+			vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+			{} // Webview options. More on these later.
+		);
+
+		panel.webview.html = getWebviewContent();
+	});
+
+	let disposable7 = vscode.commands.registerCommand('vscodeasy.copytoclipboard1', () => {
+		if (copyToAClipboard(1) == -1){
+			vscode.window.showErrorMessage('No highlighted text found!');
+		} else{
+			vscode.window.showInformationMessage("Copied to clipboard 1!")
+			treeDataProvider.refresh()
+		}
+		console.log(Codeasy_Clipboard)
+	})
+
+	let disposable8 = vscode.commands.registerTextEditorCommand('vscodeasy.pastefromclipboard1', (editor, edit) => {
+		pasteFromAClipboard(editor, edit, 1)
+	})
+
+
+	context.subscriptions.push(disposable)
+	context.subscriptions.push(disposable2)
+	context.subscriptions.push(disposable3)
+	context.subscriptions.push(disposable4)
+	context.subscriptions.push(disposable5)
+	context.subscriptions.push(disposable6);
+	context.subscriptions.push(disposable7); 
+	context.subscriptions.push(disposable8);
+
+	const treeDataProvider = new MyTreeDataProvider();
+	context.subscriptions.push(vscode.window.registerTreeDataProvider('mySidebar', treeDataProvider));
+
+	
+
+
 }
 
 function getWebViewerOptions(extensionUri) {
@@ -169,8 +169,8 @@ function generateWebViewerHTML(context, webView) {
 // Class that manages the white board
 class WhiteBoardManager {
 
-	static manager = undefined;
-	static panel =undefined;
+	// static manager = undefined;
+	// static panel = undefined;
 
 	constructor() {
 		
@@ -213,26 +213,29 @@ class MyTreeDataProvider {
 		this._onDidChangeTreeData = new vscode.EventEmitter();
 		this.onDidChangeTreeData = this._onDidChangeTreeData.event;
 		this.root = {label: "Codeasy_Clipboard", id: "root"}
-		this.children = [];
+		this.children = [
+			{label: "clipboard0", id: "0"},
+			{label: "clipboard1", id: "1"}
+		];
 	}
+
 
 	refresh() {
 		this._onDidChangeTreeData.fire();
 	}
 
     getTreeItem(element) {
-        return new vscode.TreeItem(element.label);
+		const treeItem = new vscode.TreeItem(element.label);
+        treeItem.id = element.id;
+        treeItem.collapsibleState = element.id === this.root.id ? vscode.TreeItemCollapsibleState.Expanded : vscode.TreeItemCollapsibleState.None;
+        return treeItem;
     }
 
     getChildren(element) {
 		console.log(element)
 		if(!element) return [this.root]
         if (element.id === this.root.id) {
-            return Codeasy_Clipboard.map(item => {
-				const treeItem = new vscode.TreeItem(item)
-				treeItem.id = item.id
-				return treeItem}
-				);
+            return this.children;
         }
         return [];
     }
